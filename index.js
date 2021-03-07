61c9887428c8b6c5c3deb6b0812050a1ab8ba3fd
Muge <alev.muge@gmail.com>
2021-03-07 12:00:41 +0000

    Building the App with ExpressJS and HandlebarsJS

--- index.js
+++ index.js
@@ -2,10 +2,17 @@ const express = require("express");
 const app = express();
 const hbs = require("express-handlebars");
 const path = require("path");
+const bodyParser = require("body-parser");
 const fetch = require("node-fetch");
-const getMountainView = require("./lib/onecity");
+require("dotenv").config();
+
+const indexRouter = require('./routes/indexRouter');
+const weatherRouter = require('./routes/weatherRouter');
+const errRouter = require('./routes/errRouter');
 
 app.use(express.static(path.join(__dirname, "public")));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 app.engine(
   "hbs",
@@ -16,23 +23,13 @@ app.engine(
 
 app.set("view engine", ".hbs");
 
-app.get("/", (req, res) => {
-  let data = getMountainView();
-  res.render("index", {data});
-});
+app.use("/", indexRouter);
+app.use("/weather", weatherRouter);
+app.use("*", errRouter);
 
 app.get("*", (req, res) => {
   res.render("404");
 });
-
-
-
-/* fetch(
-  "api.openweathermap.org/data/2.5/box/city?bbox={bbox}&appid={148d902626954dccada0e8334849a578}",
-  { method: 'POST', body: data})
-  .then((res) => res.json())
-  .then((json) => console.log(json)); */
-
 
 app.listen(3000, () => {
   console.log("Listening to port 3000");
